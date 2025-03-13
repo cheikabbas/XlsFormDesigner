04267231a244bce527a0b6849cd4e19369870f81
cheikabbas <cheikabbas@gmail.com>
2025-03-13 22:16:17 +0000

    Ajout de l'icone

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import wx
 from gui_home import Home
 
 if __name__ == "__main__":
+    ICON_PATH = "img/xlsform-designer-icon.ico"
     app = wx.App(False)
     frame = Home()
+    frame.SetIcon(wx.Icon(ICON_PATH, wx.BITMAP_TYPE_ICO))
     app.MainLoop()
